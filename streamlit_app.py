54c38cc10b706f2fdaeae3b958f2686abfc23a89
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-09-25 21:33:59 +0700

    add more explain

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ with st.sidebar:
         """
         <style>
         .fixed-image {
-            width: 300px; /* Ubah ukuran sesuai keinginan */
-            height: 100px; /* Ubah ukuran sesuai keinginan */
+            width: 300px;
+            height: 100px; 
         }
         </style>
         <img src="https://storage.googleapis.com/kaggle-datasets-images/130897/312329/20c79bcd928e6d481fca7d5dc9fa3ca4/dataset-cover.jpg?t=2019-05-24-07-06-55" class="fixed-image">
@@ -79,7 +79,8 @@ st.title("Analyzing Bike Sharing Data Dashboard :sparkles:")
 st.subheader("Daily Rental")
 
 total_rentals = main_df["count"].sum()
-st.metric("Total rental", value=total_rentals)
+value = f"{total_rentals} rentals"
+st.metric("Number of Rental", value=value)
 
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(
@@ -101,13 +102,20 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(
 )
 
 with tab1:
+    st.subheader("The Relationship Between Weather and The Bumber of Bicycle rentals")
+    st.write(
+        "We can use scatter plot to see the correlation pattern between the weather and count columns"
+    )
     fig, ax = plt.subplots()
     sns.scatterplot(x=main_df["weather"], y=main_df["count"], ax=ax)
     sns.regplot(x=main_df["weather"], y=main_df["count"], ax=ax, scatter=False)
     ax.set_title("The relationship between weather and the number of bicycle rentals")
     st.pyplot(fig)
+    st.write("The image above shows that the correlation between the two columns is very small.")
 
 with tab2:
+    st.subheader("Number and Average of Tenants by Working Day")
+    st.write("We will use a bar plot to see the comparison between the average and the number of tenants on weekdays and non-weekdays.")
     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(24, 30))
     colors = ["#72BCD4", "#D3D3D3"]
 
@@ -119,7 +127,7 @@ with tab2:
         ax=ax[0],
     )
     ax[0].set_ylabel(None)
-    ax[0].set_xlabel("Working day(1 = Workday)", fontsize=30)
+    ax[0].set_xlabel("Working day(1 = weekday)", fontsize=30)
     ax[0].set_title("Average tenants by working day", loc="center", fontsize=36)
     ax[0].tick_params(axis="y", labelsize=28)
     ax[0].tick_params(axis="x", labelsize=28)
@@ -132,10 +140,8 @@ with tab2:
         ax=ax[1],
     )
     ax[1].set_ylabel(None)
-    ax[1].set_xlabel("Working day(1 = Workday)", fontsize=30)
-    ax[1].set_title(
-        "Number of tenants based on working days", loc="center", fontsize=36
-    )
+    ax[1].set_xlabel("Working day(1 = weekday)", fontsize=30)
+    ax[1].set_title("Number of tenants based on working days", loc="center", fontsize=36)
     ax[1].tick_params(axis="y", labelsize=28)
     ax[1].tick_params(axis="x", labelsize=28)
 
@@ -148,7 +154,11 @@ with tab2:
 
     st.pyplot(fig)
 
+    st.write("Weekdays show larger numbers and averages on weekdays")
+
 with tab3:
+    st.subheader("Number of Rentals Rrom Registered Renters Ruring Reekends and Reekdays Rase Rn Renant Rype")
+    st.write("we can use plot points to see the daily borrowing pattern in hours based on tenant type (casual, registered)")
     fig1, ax1 = plt.subplots()
     sns.pointplot(data=main_df, x="hour", y="casual", hue="weekday", ax=ax1)
     ax1.set_title("Number of rentals from casual renters during weekends and weekdays")
@@ -157,15 +167,18 @@ with tab3:
 
     fig2, ax2 = plt.subplots()
     sns.pointplot(data=main_df, x="hour", y="registered", hue="weekday", ax=ax2)
-    ax2.set_title(
-        "Number of rentals from registered renters during weekends and weekdays"
-    )
+    ax2.set_title("Number of rentals from registered renters during weekends and weekdays")
 
     st.pyplot(fig2)
+
+    st.write("The above pattern shows that casual renters rent more on weekends, while registered renters rent more on weekdays.")
 
 # teknik analisis lanjutan
 
 with tab4:
+    st.subheader("Using Clustering Techniques to See Clusters From A Dataset")
+    st.write("Use clustering techniques to see clusters of the dataset based on hour, season, weather, temperature, humidity, etc.")
+
     from sklearn.preprocessing import StandardScaler
 
     # cari jumlah cluster optimal
@@ -193,13 +206,6 @@ with tab4:
         kmeans = KMeans(n_clusters=k, random_state=42)
         kmeans.fit(data_scaled)
         sse.append(kmeans.inertia_)
-
-    fig1, ax1 = plt.subplots()
-    ax1.plot(range(1, 11), sse)
-    ax1.set_xlabel("Number of Clusters")
-    ax1.set_ylabel("SSE")
-    ax1.set_title("Finding the optimal number of clusters with the Elbow Method")
-    st.pyplot(fig1)
 
     # tampilkan scatter dari cluster
     kmeans = KMeans(n_clusters=4, random_state=42)
@@ -238,7 +244,19 @@ with tab4:
     # Menampilkan tabel di Streamlit
     st.dataframe(data=filtered_cluster_df, width=700)
 
+    st.markdown(
+        """
+        From the results of grouping/clustering, it is divided into 4 clusters, namely:
+- `Cluster 0` which can be defined that this cluster represents a weekday in the `afternoon in summer, with sunny weather and fairly warm temperatures`. The majority of users are `registered users` compared to `casual users`. This indicates that this cluster involves busy times during the after-work hours, with quite high bicycle use by regular users.
+- `Cluster 1` which can be defined that this cluster describes `afternoon in spring, with cool temperatures and sunny weather`. Bicycle use by casual users is lower, and the majority of users are registered. This shows quite low activity compared to the previous cluster, perhaps because of the non-peak hours, or the cooler weather in spring.
+- `Cluster 2` which can be defined that this cluster occurs in the `afternoon in summer with relatively warm temperatures`. This cluster may represent very busy times with a high total number of cyclists. This is likely to be the end of the work day or early evening when many people, both regular and occasional users, rent bikes. 
+- `Cluster 3` which can be defined as representing `very early winter mornings with cold temperatures and high humidity`. Both casual and registered users are very low, indicating very little time spent on bicycles, perhaps due to the very early morning, cold weather and less than ideal conditions for cycling.
+        """
+    )
+
 with tab5:
+    st.subheader("Correlation Between Tables")
+    st.write("Use heatmaps to display more interesting visualizations so that the correlation between tables can be seen clearly.")
     heatmap_df = main_df.drop(columns=["instant", "dateday", "year"], axis=1)
     correlation_values = heatmap_df.corr()
     fig, ax = plt.subplots(figsize=(15,15))
